565320d8e54dcb1b3c8b050b294ef9195f3dd6b1
y_uuki <yuki.tsubo@gmail.com>
2020-10-05 15:53:55 +0900

    Update fish abbr

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ abbr -a q exit
 abbr -a v nvim
 abbr -a c code
 abbr -a s ssh
+abbr -a gcs gcloud compute ssh
 
 abbr -a g git
 abbr -a gst git status
@@ -13,12 +14,15 @@ abbr -a gv gh repo view --web
 
 abbr -a k kubectl
 abbr -a d docker
+abbr -a dm docker-machine
+abbr -a dc docker-compose
 
 abbr -a mb make build
 abbr -a mt make test
 
 abbr -a ll ls -lh
 abbr -a la ls -a
+abbr -a cp cp -p
 
 gabbr --add G '| grep'
 gabbr --add H '| head'
@@ -31,3 +35,7 @@ gabbr --add ..... '..//..//..//..'
 ### Key bindings
 
 bind \cxb fzf_git_recent_branch
+
+### Enviroment variables
+
+eval (direnv hook fish)
