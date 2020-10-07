48784bed59329d6544428304ac14c84e0b44ac8f
y_uuki <yuki.tsubo@gmail.com>
2020-10-07 14:54:17 +0900

    Update ghq keybindings

--- fish/conf.d/ghq_key_bindings.fish
+++ fish/conf.d/ghq_key_bindings.fish
@@ -1,4 +1,4 @@
-bind \cg '__ghq_repository_search'
+bind \cs '__ghq_repository_search'
 if bind -M insert >/dev/null 2>/dev/null
-    bind -M insert \cg '__ghq_repository_search'
+    bind -M insert \cs '__ghq_repository_search'
 end
